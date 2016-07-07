08a04d45ddd6325f41ad3d10b0b0ecc9de983d76
mpasek <MikeP@MP.attlocal.net>
2016-07-06 17:23:29 -0700

    Initial Commit

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($){
+	//open-close submenu on mobile
+	$('.main-nav').on('click', function(event){
+		if($(event.target).is('.main-nav')) $(this).children('ul').toggleClass('is-visible');
+	});
+});
